f55fb1d41ae0d0bf537f0a11c98450f9a073a88d
Aldrich <118922910+Fetalys@users.noreply.github.com>
2025-04-15 20:47:50 +0800

    time.js

--- time.js
+++ time.js
@@ -2,11 +2,11 @@ function updateClock() {
     const options = { timeZone: 'Asia/Manila', hour: '2-digit', minute: '2-digit', second: '2-digit' };
     const now = new Date();
     const timeString = now.toLocaleTimeString('en-PH', options);
-    document.getElementById('clock').innerText = timeString;
+    document.getElementById('clock-body').innerText = timeString;
 }
 
-// Update the clock every second
+// update the clock every second
 setInterval(updateClock, 1000);
 
-// Initial call to display the clock immediately
+// initial call to display the clock immediately
 updateClock();
